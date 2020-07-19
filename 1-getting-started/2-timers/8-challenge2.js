65eb628f4bb2dc4fb167d69d2bc35fc84ce2c70d
HENRY-KC <henrykc24@yahoo.com>
2020-07-19 20:35:55 +0100

    solve first challenge

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,12 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let counter = 1;
+const intervalID = setInterval(() => {
+    console.log('Hello World');
+    if( counter === 5 ) {
+      clearInterval(intervalID);
+    } else {
+      counter++;
+    }
+  },1 * 1000);
